985f35dbc883a8c2349e8c14c4d45864ad477223
jasmine-ah <yasminip30@gmail.com>
2024-12-05 14:27:21 +0300

    nodemodules updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from './pages/NavBar.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Login from './pages/Login.jsx';
 import Blogpost from './pages/Blogpost.jsx';
-import DisplayPost from './pages/Displaypost.jsx';
+import DisplayPost from './pages/DisplayPost.jsx';
 import MyBlog from './pages/MyBlog.jsx';
 
 function App() {
